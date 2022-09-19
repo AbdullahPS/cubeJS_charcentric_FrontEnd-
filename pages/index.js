a4f362351ffd0c12209e6c2405f44c121d9ce8c2
AbdullahPS <abdullahtabbal@outlook.com>
2022-09-19 19:10:49 +0300

    HomePage: Add initial Navigation Bar Layout

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Head from "next/head"
+import Image from "next/image"
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Charcentric </title>
         <meta name="description" content="A simple charcentric Cube.js application" />
       </Head>
 
-      <main className={styles.main}>
-       
+    <nav class="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+<div>
+  <span class="text-2xl font-sans">
+  <Image src={'/logo.png'} width="50px" height="50px"></Image>
+</span> 
+</div>
+<ul class="md:flex md:items-center">
+  <li class="mx-4">
+    <a href="#" class="text-xl hover:text-cyan-500 duration-500">HOME</a>
+  </li>
+  <li class="mx-4">
+    <a href="#" class="text-xl hover:text-cyan-500 duration-500">ABOUT</a>
+  </li>
+  <li class="mx-4">
+    <a href="#" class="text-xl hover:text-cyan-500 duration-500">EXIT</a>
+  </li>
+</ul>
+
+    </nav>
+      <main class="bg-cyan-400">
+    
       </main>
 
-      <footer className={styles.footer}>
+      <footer >
        
       </footer>
-    </div>
+      </>
   )
 }
